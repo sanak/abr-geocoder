b5ea1c16877ddcc716b3489f7c2902bfea3c9f26
Ko Nagase <nagase@georepublic.co.jp>
2024-01-19 02:15:41 +0900

    Fix system test error
    Adjust dummy sqlite database path

--- src/abrg-data-source.ts
+++ src/abrg-data-source.ts
@@ -4,8 +4,8 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 export const commonOptions = {
   synchronize: false,
   logging: false,
-  entities: ['src/entity/*.ts'],
-  migrations: ['src/migration/*.ts'],
+  entities: ['dist/entity/*.js'],
+  migrations: ['dist/migration/*.js'],
   migrationsRun: true,
 };
 
@@ -13,7 +13,7 @@ const sqliteOptions: DataSourceOptions = {
   ...commonOptions,
   type: 'better-sqlite3',
   // src/interface-adapter/providers/provide-data-source.ts 内で絶対パスに置換
-  database: '~/.abr-geocoder/ba000001.sqlite',
+  database: 'ba000001.sqlite',
 };
 
 const options: DataSourceOptions = sqliteOptions;
